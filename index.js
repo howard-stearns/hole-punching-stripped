fed7a7c9ae55917f3f1fc3d629e23148d8d7b919
howard-stearns <howard@highfidelity.io>
2020-01-22 10:05:39 -0800

    heroku port management

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var server = require('net').createServer(function (socket) {
     console.log('> Connect to this public endpoint with clientB:', socket.remoteAddress + ':' + socket.remotePort);
-}).listen(9999, function (err) {
+}).listen(process.env.PORT || 5000, function (err) {
     if (err) return console.log(err);
     console.log('> (server) listening on:', server.address().address + ':' + server.address().port)
 });
