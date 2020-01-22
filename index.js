aa737b7da851dcbb3a1bc83358e7b7d72f7ce06a
howard-stearns <howard@highfidelity.io>
2020-01-22 10:29:55 -0800

    log received data

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 'use strict';
+const HANDSHAKE = "c'est moi\n";
 
 var server = require('net').createServer(function (socket) {
     console.log('> connection from:', socket.remoteAddress + ':' + socket.remotePort);
     socket.on('data', function (data) {
-        if (data === "c'est moi") {
+        if (data === HANDSHAKE) {
             console.log('> Connect to this public endpoint with clientB:', socket.remoteAddress + ':' + socket.remotePort);
+        } else {
+            console.log('got data from', socket.remoteAddress + ':' + socket.remotePort, data.length 'characters:', data);
         }
     });
 }).listen(process.env.PORT || 5000, function (err) {
