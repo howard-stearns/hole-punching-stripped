af57e5f304773089b691fdc67130978f3494e918
howard-stearns <howard@highfidelity.io>
2020-01-22 10:23:56 -0800

    handshake to recognize connections from us, instead of random people knocking on our door

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 'use strict';
 
 var server = require('net').createServer(function (socket) {
-    console.log('> Connect to this public endpoint with clientB:', socket.remoteAddress + ':' + socket.remotePort);
+    console.log('> connection from:', socket.remoteAddress + ':' + socket.remotePort);
+    socket.on('data', function (data) {
+        if (data === "c'est moi") {
+            console.log('> Connect to this public endpoint with clientB:', socket.remoteAddress + ':' + socket.remotePort);
+        }
+    });
 }).listen(process.env.PORT || 5000, function (err) {
     if (err) return console.log(err);
     console.log('> (server) listening on:', server.address().address + ':' + server.address().port)
